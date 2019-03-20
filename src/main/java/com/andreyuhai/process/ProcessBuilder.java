1b73a5ed2f6f52aaa0a263063e3e5f71d02bb9cc
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-03-20 16:24:13 +0100

    Helper methods added to create preset process

--- src/main/java/com/andreyuhai/process/ProcessBuilder.java
+++ src/main/java/com/andreyuhai/process/ProcessBuilder.java
@@ -1,10 +1,14 @@
 package com.andreyuhai.process;
 
+import com.andreyuhai.processor.Processor;
+
 public class ProcessBuilder {
 
     private int pid;
     private int burst_time;
     private int arrival_time;
+    private boolean preset;
+    private Processor processor;
 
 
     public ProcessBuilder() { }
@@ -19,12 +23,22 @@ public class ProcessBuilder {
         return this;
     }
 
+    public ProcessBuilder preset(boolean preset) {
+        this.preset = preset;
+        return this;
+    }
+
     public ProcessBuilder arrival_time(int arrival_time) {
         this.arrival_time = arrival_time;
         return this;
     }
 
+    public ProcessBuilder processor(Processor processor) {
+        this.processor = processor;
+        return this;
+    }
+
     public Process build() {
-        return new Process(pid, arrival_time, burst_time);
+        return new Process(processor, pid, burst_time, arrival_time, preset);
     }
 }
