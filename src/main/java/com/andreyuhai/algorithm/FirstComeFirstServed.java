951c83a93da11cab8fb062fca32fff032d669d9b
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-03-20 05:44:19 +0100

    tim.incrementAndGet() has been placed on the correct line to avoid time confusion

--- src/main/java/com/andreyuhai/algorithm/FirstComeFirstServed.java
+++ src/main/java/com/andreyuhai/algorithm/FirstComeFirstServed.java
@@ -33,12 +33,12 @@ public class FirstComeFirstServed extends SchedulingAlgorithm {
             // Trying to simulate real life clock so in that 10 ms sleep time
             // other processes can enter the ready queue at an arbitrary time.
             for(int i = 0; i < currentProcess.getBurst_time(); i++) {
-                time.incrementAndGet();
                 try {
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                time.incrementAndGet();
             }
 
             totalWaitTime += executeProcess(currentProcess);
